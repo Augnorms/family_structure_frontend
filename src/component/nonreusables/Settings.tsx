4b265d1646b3ea55b7e2a358c506ad902af74675
Augustine Normanyo <augustine.normanyo@amalitech.com>
2023-12-30 15:11:27 +0100

    refactor codes for dashboard upate three

--- src/component/nonreusables/Settings.tsx
+++ src/component/nonreusables/Settings.tsx
@@ -1,21 +1,38 @@
-
+import { useContext, useEffect, useState } from "react"
+import {loggedinInfoContext} from "../../contextApi/LoggedInInforContext";
+import { formatTime } from "../../HelperFunction/functions";
 
 export const Settings = () => {
+
+const {tokenExp} = useContext(loggedinInfoContext);
+
+const [countdown, setCountdown] = useState<number>(0);
+
+useEffect(() => {
+  let intervalId: number;
+
+  if (tokenExp > 0) {
+    setCountdown(Math.floor(tokenExp / 1000));
+
+    intervalId = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+  }
+
+  return () => {
+    clearInterval(intervalId);
+  };
+}, [tokenExp]);
+
   return (
-    <div className='w-full h-screen  p-2'>
+    <div className='w-full h-screen p-2 overflow-auto'>
 
         <div className="w-full p-2 flex border">
             <div className="w-1/2 ">
-                settings
+                settings{}
             </div>
             <div className="w-1/2 flex justify-end">
-            <div className="w-[20%] max-sm:hidden sm:hidden lg:block">
-        
-            </div>
-
-            <div className="max-sm:w-[20%] sm:w-[20%] w-[10%] lg:hidden">
-                
-            </div>
+              <p><span className="font-bold">LoggedIn Time:</span> <span className="text-cyan-500 font-bold">{" "+formatTime(countdown)}</span></p>
             </div>
         </div>
 
