b2c48bcb5d6028aa31234d3a639686b551df3714
Augustine Normanyo <augustine.normanyo@amalitech.com>
2024-01-09 15:14:01 +0100

    added extra queries

--- src/component/nonreusables/Home.tsx
+++ src/component/nonreusables/Home.tsx
@@ -5,6 +5,8 @@ import { RoundedProgressBar } from "../reusables/nonformcomponent/RoundedProgres
 export const Home = () => {
 
   const[totalmem, setTotalmem] = useState<number>(0);
+  const[totalfemale, setTotalfemale] = useState<number>(0);
+  const[totalmale, setTotalmale] = useState<number>(0);
   const[loading, setLoading] = useState<boolean>(false);
  
   const handlemembercount = async()=>{
@@ -23,8 +25,44 @@ export const Home = () => {
      }
   }; 
 
+  //handle female count
+  const handlefemalecount = async()=>{
+    try{
+      setLoading(false);
+      const response = await axios.get("http://localhost:4000/allfemalecount");
+
+      if(response && response?.data?.code === 200){
+        setTotalfemale(response?.data?.count);
+      }
+
+    }catch(err:any){
+      console.error(err.response);
+    }finally{
+      setLoading(false);
+    }
+  };
+
+  //handle male count
+  const handlemalecount = async()=>{
+    try{
+      setLoading(false);
+      const response = await axios.get("http://localhost:4000/allmales");
+
+      if(response && response?.data?.code === 200){
+        setTotalmale(response?.data?.count);
+      }
+
+    }catch(err:any){
+      console.error(err.response);
+    }finally{
+      setLoading(false);
+    }
+  };
+
   useEffect(()=>{
     handlemembercount();
+    handlefemalecount();
+    handlemalecount();
   },[]);
 
   return (
@@ -68,12 +106,20 @@ export const Home = () => {
 
          </div>
 
-         <div className="h-[18vh] shadow-lg border-t-4 border-cyan-700 rounded-sm hover:scale-y-110">
-
+         <div className="h-[18vh] p-2 shadow-lg border-t-4 border-cyan-700 rounded-sm hover:scale-y-110">
+            <p>
+              <span className="font-bold">Females:</span> 
+              <span className="text-cyan-600 ml-2 font-bold">{totalfemale}</span>
+            </p>
+           <RoundedProgressBar progressFigure={totalfemale} centerLabel={totalfemale}/>
          </div>
 
-         <div className="h-[18vh] shadow-lg border-t-4 border-cyan-700 rounded-sm hover:scale-y-110">
-
+         <div className="h-[18vh] p-2 shadow-lg border-t-4 border-cyan-700 rounded-sm hover:scale-y-110">
+            <p>
+              <span className="font-bold">Males:</span> 
+              <span className="text-cyan-600 ml-2 font-bold">{totalmale}</span>
+            </p>
+           <RoundedProgressBar progressFigure={totalmale} centerLabel={totalmale}/>
          </div>
 
          <div className="h-[18vh] shadow-lg border-t-4 border-cyan-700 rounded-sm hover:scale-y-110">
